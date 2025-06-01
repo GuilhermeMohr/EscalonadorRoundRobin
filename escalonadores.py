1cff461a902d084cca30553968bef4b89ea9d8b5
gabrielpassosduettsoftware <gabriel.francisco@duettsoftware.com>
2025-06-01 19:54:26 -0300

    feat: implementação da linha do tempo de Gantts e quantums dinâmicos

--- escalonadores.py
+++ escalonadores.py
@@ -10,6 +10,7 @@ class EventProcess:
         self.runEvent = runEvent
         self.dataRow = dataRow
         self.segundosTotaisAteFinalizacao = Value('d', 0.0)
+        self.quantum = 1
 
     runEvent : Event # type: ignore
     process : Process
@@ -56,7 +57,10 @@ def finish():
             process.segundosTotaisAteFinalizacao.value - tempoExecutando,
             process.trocasContextoTotais
         ])
-    
+    print('\nLinha do tempo Gantt:')
+    print("{:<7} {:<10} {:<10} {:<10} {:<10}".format("Tempo", "Core1", "Core2", "Core3", "Core4"))
+    for entry in timeline:
+        print("{:<7} {:<10} {:<10} {:<10} {:<10}".format(*entry))
     for row in data:
         print("{:<5} {:<20} {:<20} {:<20}".format(*row))
 
@@ -81,6 +85,7 @@ def finish():
     exit()
 
 if __name__ == '__main__':
+    timeline = []
     beginTime = time.time()
     from multiprocessing import set_start_method
     set_start_method('spawn')
@@ -131,7 +136,7 @@ if __name__ == '__main__':
                 if not processes[0].process.is_alive() and not (processes[0].process.exitcode is None):
                     completeProcesses.append(processes[0])
                     processes.pop(0)
-                    runCicle(processes)
+                    return runCicle(processes)
                 else:
                     if processes[0].runEvent.is_set():
                         processes[0].trocasContextoTotais += 1
@@ -140,17 +145,33 @@ if __name__ == '__main__':
                         processes.pop(0)
                         processes.append(process)
                     processes[0].runEvent.set()
+                if quantumOpcoes == 'a':
+                    processes[0].quantum = random.randint(1, 3)
+                else:
+                    processes[0].quantum = 2
+                return processes[0].quantum
 
         while not threadStopEvent.is_set(): 
-            runCicle(cpuCore1Process)
-            runCicle(cpuCore2Process)
-            runCicle(cpuCore3Process)
-            runCicle(cpuCore4Process)
+            core1 = cpuCore1Process[0].dataRow.id if cpuCore1Process and cpuCore1Process[0].runEvent.is_set() else '-'
+            core2 = cpuCore2Process[0].dataRow.id if cpuCore2Process and cpuCore2Process[0].runEvent.is_set() else '-'
+            core3 = cpuCore3Process[0].dataRow.id if cpuCore3Process and cpuCore3Process[0].runEvent.is_set() else '-'
+            core4 = cpuCore4Process[0].dataRow.id if cpuCore4Process and cpuCore4Process[0].runEvent.is_set() else '-'
 
-            if quantumOpcoes == 'f':
-                time.sleep(2)
+            # Executa clico da CPU de cada core
+            quantum1 = runCicle(cpuCore1Process)
+            quantum2 = runCicle(cpuCore2Process)
+            quantum3 = runCicle(cpuCore3Process)
+            quantum4 = runCicle(cpuCore4Process)
+
+            elapsed_time = int(time.time() - beginTime)
+            timeline.append([elapsed_time, core1, core2, core3, core4])
+            quantums = [q for q in [quantum1, quantum2, quantum3, quantum4] if q is not None]
+
+            if quantums:
+                time.sleep(max(quantums))
             else:
-                time.sleep(random.randint(1,5))
+                time.sleep(1)
+
 
     threadStopEvent = threading.Event()
 
