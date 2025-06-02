8a5296a5db7dfb4bb1cac3640a895f4639ac0431
Gabriel-Steps <gabrielpassosfrancisco@gmail.com>
2025-06-01 21:14:12 -0300

    feat update: abstração de processBuild

--- escalonadores.py
+++ escalonadores.py
@@ -1,9 +1,9 @@
 from multiprocessing import Process, Event, Value, current_process
 import time
 import random
-import time
 from typing import Any, List
 import readProcesses
+import threading
 
 class EventProcess:
     def __init__(self, runEvent, dataRow):
@@ -11,7 +11,7 @@ class EventProcess:
         self.dataRow = dataRow
         self.segundosTotaisAteFinalizacao = Value('d', 0.0)
 
-    runEvent : Event # type: ignore
+    runEvent : Event  # type: ignore
     process : Process
     dataRow : readProcesses.DataRow
     trocasContextoTotais = 0
@@ -57,7 +57,10 @@ def finish():
             process.segundosTotaisAteFinalizacao.value - tempoExecutando,
             process.trocasContextoTotais
         ])
-    
+    print('\nLinha do tempo Gantt:')
+    print("{:<7} {:<10} {:<10} {:<10} {:<10}".format("Tempo", "Core1", "Core2", "Core3", "Core4"))
+    for entry in timeline:
+        print("{:<7} {:<10} {:<10} {:<10} {:<10}".format(*entry))
     for row in data:
         print("{:<5} {:<20} {:<20} {:<20}".format(*row))
 
@@ -80,9 +83,23 @@ def finish():
     processBuilder.join()
     for processRunner in processRunners:
         processRunner.join()
+    timelineThread.join()
     exit()
 
+def timeline_updater(threadStopEvent):
+    while not threadStopEvent.is_set():
+        tempoAtual = int(time.time() - beginTime)
+        linha = [str(tempoAtual)]
+        for coreList in [cpuCore1Process, cpuCore2Process, cpuCore3Process, cpuCore4Process]:
+            if len(coreList) > 0 and coreList[0].runEvent.is_set():
+                linha.append(f"P{coreList[0].dataRow.id}")
+            else:
+                linha.append("-")
+        timeline.append(linha)
+        time.sleep(1)  # atualiza a cada 1 segundo
+
 if __name__ == '__main__':
+    timeline = []
     beginTime = time.time()
     from multiprocessing import set_start_method
     set_start_method('spawn')
@@ -103,7 +120,6 @@ if __name__ == '__main__':
         newProcess.process = Process(target=process, args=(newProcess, stopEvent))
         processes.append(newProcess)
     
-    import threading
     def processBuild(threadStopEvent : threading.Event, processes : List[EventProcess]):
         while not threadStopEvent.is_set():
             leastTime = 0
@@ -124,17 +140,16 @@ if __name__ == '__main__':
                 elif process.dataRow.tempoChegada > leastTime:
                     leastTime = process.dataRow.tempoChegada - nowTime
 
-            processes = [process for process in processes if process.dataRow.tempoChegada >= nowTime]
-            time.sleep(leastTime)
+            processes[:] = [process for process in processes if process.dataRow.tempoChegada >= nowTime]
+            time.sleep(min(leastTime, 1))
 
     def processRunn(cpuProcesses : List[EventProcess], threadStopEvent : threading.Event, quantumOpcoes : str):
         def runCicle(processes : list[EventProcess]):
             if len(processes) != 0:
-                if not processes[0].process.is_alive() and not (processes[0].process.exitcode is None):
-                    processes[0].quantum -= 1
+                if not processes[0].process.is_alive():
                     completeProcesses.append(processes[0])
                     processes.pop(0)
-                    runCicle(processes)
+                    return runCicle(processes)
                 else:
                     if processes[0].runEvent.is_set():
                         processes[0].trocasContextoTotais += 1
@@ -143,9 +158,18 @@ if __name__ == '__main__':
                         processes.pop(0)
                         processes.append(process)
                     processes[0].runEvent.set()
+                if quantumOpcoes == 'a':
+                    processes[0].quantum = random.randint(1, 3)
+                else:
+                    processes[0].quantum = 2
+                return processes[0].quantum
 
         while not threadStopEvent.is_set(): 
             runCicle(cpuProcesses)
+
+            if len(cpuProcesses) == 0:
+                time.sleep(1)
+                continue
 
             if quantumOpcoes == 'f':
                 time.sleep(2)
@@ -165,6 +189,10 @@ if __name__ == '__main__':
 
     processBuilder = threading.Thread(target=processBuild, args=(threadStopEvent, processes))
     processBuilder.start()
+
+    timelineThread = threading.Thread(target=timeline_updater, args=(threadStopEvent,))
+    timelineThread.start()
+
     processRunners : List[threading.Thread] = [
         threading.Thread(target=processRunn, args=(cpuCore1Process, threadStopEvent, quantumOpcoes)),
         threading.Thread(target=processRunn, args=(cpuCore2Process, threadStopEvent, quantumOpcoes)),
